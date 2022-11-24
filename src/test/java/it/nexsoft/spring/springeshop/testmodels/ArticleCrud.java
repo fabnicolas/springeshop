87011b5b416dfc9a835781a5834fae94f1ea4d49
rikon311 <polidororiccardo84@gmail.com>
2022-11-24 15:55:11 +0100

    M-00003: aggiunta configurazione per testing tramite database dedicato

--- src/test/java/it/nexsoft/spring/springeshop/testmodels/ArticleCrud.java
+++ src/test/java/it/nexsoft/spring/springeshop/testmodels/ArticleCrud.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +33,7 @@ import it.nexsoft.spring.springeshop.repositories.ArticleRepository;
 @AutoConfigureMockMvc
 @WithMockUser
 @Transactional
+@ActiveProfiles("test")
 public class ArticleCrud {
 
 	private static final String ENTITY_API_URL = "/prova/";
