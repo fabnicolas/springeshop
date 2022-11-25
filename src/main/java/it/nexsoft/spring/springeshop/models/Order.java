5bd457ba9777e35de80e769ae5def6aa3fef26ae
rikon311 <polidororiccardo84@gmail.com>
2022-11-25 13:26:46 +0100

    M-00004: Aggiunti campi data e prezzo per l'ordine

--- src/main/java/it/nexsoft/spring/springeshop/models/Order.java
+++ src/main/java/it/nexsoft/spring/springeshop/models/Order.java
@@ -1,5 +1,8 @@
 package it.nexsoft.spring.springeshop.models;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,5 +26,61 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "article_id")
 	private Article article;
+
+	@Column(name = "order_date")
+	private LocalDate orderDate;
+
+	@Column(name = "price")
+	private Double price;
+
+	public Order() {
+	}
+
+	public Order(final User user, final Article article, final LocalDate orderDate, final Double price) {
+		this.user = user;
+		this.article = article;
+		this.orderDate = orderDate;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(final User user) {
+		this.user = user;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(final Article article) {
+		this.article = article;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(final LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(final Double price) {
+		this.price = price;
+	}
 
 }
